51a2b9bf24471f2c0d5cb9ed6af5bea48321e852
OPR_A <opr.a.ruby@gmail.com>
2020-02-18 23:32:43 +0400

    DZ-1 post for /visit and data to ./public/users.txt

--- app.rb
+++ app.rb
@@ -15,6 +15,23 @@ get '/visit' do
 	erb :visit
 end
 
+post '/visit' do
+	@user_name = params[:user_name]
+    @phone     = params[:phone]
+    @date_time = params[:date_time]
+
+    @title = 'Спасибо!'
+    @message = "Спасибо вам, #{@user_name}, будем ждать Вас."
+
+    out_f = File.open './public/users.txt', 'a'
+    out_f.write "User: #{@user_name}, Phone: #{@phone},"
+    out_f.write " Date_Time: #{@date_time}\n"
+    out_f.close
+
+    erb :message
+    
+end
+
 get '/contacts' do
 	erb :contacts
 end
